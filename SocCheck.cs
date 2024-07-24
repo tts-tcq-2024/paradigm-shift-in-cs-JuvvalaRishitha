b2ed1d10eafeae89e66eb8c280cad43e4565c2de
JuvvalaRishitha <juvvalarishitha@gmail.com>
2024-07-24 15:23:24 +0530

    Update SocCheck.cs

--- SocCheck.cs
+++ SocCheck.cs
@@ -14,9 +14,14 @@ namespace Checker
         /// <returns>True if the state of charge is within the range, otherwise false.</returns>
         public bool Check(float stateOfCharge)
         {
-            if (stateOfCharge < 20 || stateOfCharge > 80)
+            if (stateOfCharge < 20)
             {
-                _errorMessage = "State of Charge is out of range!";
+                _errorMessage = "State of Charge is too low!";
+                return false;
+            }
+            if (stateOfCharge > 80)
+            {
+                _errorMessage = "State of Charge is too high!";
                 return false;
             }
             return true;
